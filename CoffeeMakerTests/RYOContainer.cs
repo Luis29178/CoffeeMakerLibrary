f9276b6696216ec5c2c49ac1a08ebc3bb4b852f4
Luis Bazaldua <LEBazaldua@student.fullsail.edu>
2022-04-28 00:43:23 -0400

    Add project files.

---
+++ CoffeeMakerTests/RYOContainer.cs
@@ -0,0 +1,58 @@
+﻿using CoffeeMakerLibrary;
+
+namespace CoffeeMakerTests
+{
+    public class RYOContainer : IContainer
+    {
+        public int volume;
+        public bool IsEmpty { get
+            {
+                if (volume == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public void Clean()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DispensePortion(int portion)
+        {
+            if (volume <= portion)
+            {
+                volume -= portion; 
+            }
+            else
+            {
+                throw new System.InvalidOperationException();
+            }
+        }
+
+        public void Empty()
+        {
+            volume = 0;
+        }
+
+        public void Fill()
+        {
+            volume = 10;
+        }
+
+        public int GetVolume()
+        {
+            return volume;
+        }
+
+        public void SetVolume(int volume)
+        {
+            this.volume = volume;
+        }
+    }
+}
